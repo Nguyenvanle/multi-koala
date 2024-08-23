7a10127fb5222bbd14a7e811ef24dce72bf3fcf4
Tú Lê <leduonganhtu0102@gmail.com>
2024-08-23 10:02:50 +0700

    SetUp: Expo ReactNative

---
+++ mobile/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Styles from "@/constants/Styles";
+
+const Index = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Index;
---
+++ mobile/constants/Styles.ts
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+  },
+});
+
+export default Styles;
