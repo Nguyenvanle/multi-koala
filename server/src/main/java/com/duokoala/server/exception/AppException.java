8e8a979eef83f5baa98c25e2517b8689636b3fc5
khachhoangthienn <tiendat29082003@gmail.com>
2024-08-24 14:28:27 +0700

    add: catch and solve exception

---
+++ server/src/main/java/com/duokoala/server/exception/AppException.java
@@ -0,0 +1,18 @@
+package com.duokoala.server.exception;
+
+
+public class AppException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
