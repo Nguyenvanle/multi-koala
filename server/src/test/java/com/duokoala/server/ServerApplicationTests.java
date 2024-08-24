84ef2e6728327e99965204b7251c1027c1b1dc25
khachhoangthienn <tiendat29082003@gmail.com>
2024-08-24 11:20:40 +0700

    import server into github

---
+++ server/src/test/java/com/duokoala/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duokoala.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
