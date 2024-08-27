43ed963e8bde62af93e4f04cf0f2de85e54daf99
khachhoangthienn <tiendat29082003@gmail.com>
2024-08-27 18:39:52 +0700

    add/server/auth: custom preauthorize for permission

--- server/src/main/java/com/duokoala/server/configuration/CustomJwtDecoder.java
+++ server/src/main/java/com/duokoala/server/configuration/CustomJwtDecoder.java
@@ -1,14 +1,21 @@
 package com.duokoala.server.configuration;
 
+import com.duokoala.server.dto.request.authRequest.IntrospectRequest;
 import com.duokoala.server.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.experimental.NonFinal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.text.ParseException;
+import java.util.Objects;
 
 //decoder jwt
 //manage can use this jwt access to this system
@@ -25,6 +32,23 @@ public class CustomJwtDecoder implements JwtDecoder {
     private NimbusJwtDecoder nimbusJwtDecoder = null;
 
     public Jwt decode(String token) throws JwtException {
-        return null;
+        try {
+            var response = authenticationService.introspect(
+                    IntrospectRequest.builder().token(token).build());
+            if (!response.isValid())
+                throw new JwtException("Token invalid");
+        } catch (ParseException | JOSEException e) {
+            throw new JwtException(e.getMessage());
+        }
+
+        if(Objects.isNull(nimbusJwtDecoder)) {
+            SecretKeySpec secretKeySpec =
+                    new SecretKeySpec(signerKey.getBytes(),"HS512");
+            nimbusJwtDecoder = NimbusJwtDecoder
+                    .withSecretKey(secretKeySpec)
+                    .macAlgorithm(MacAlgorithm.HS512)
+                    .build();
+        } //create authority for user access to system
+        return nimbusJwtDecoder.decode(token);
     }
 }
--- server/src/main/java/com/duokoala/server/configuration/SecurityConfig.java
+++ server/src/main/java/com/duokoala/server/configuration/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.duokoala.server.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,8 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.security.Security;
 
 @Configuration
 @EnableWebSecurity
@@ -32,25 +28,26 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request ->
                 request.requestMatchers(/*HttpMethod.POST,*/PUBLIC_ENDPOINTS).permitAll()
-                        .anyRequest().authenticated()
-        );
+                        .anyRequest().authenticated());
+
         httpSecurity.oauth2ResourceServer(oauth2 ->
                 oauth2.jwt(jwtConfigurer ->
                         jwtConfigurer.decoder(customJwtDecoder)
-//                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
                                 ));
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
-//    @Bean
-//    JwtAuthenticationConverter jwtAuthenticationConverter() {
-//        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-//        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
-//        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
-//        return jwtAuthenticationConverter;
-//    }
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+        //without discard ROLE_ init permission
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
