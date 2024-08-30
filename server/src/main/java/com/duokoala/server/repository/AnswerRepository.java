a62c43185e88be16e69b879c46e0b0d2c188c5ef
khachhoangthienn <tiendat29082003@gmail.com>
2024-08-30 16:15:11 +0700

    add/server: create new entity & repository answer

--- server/src/main/java/com/duokoala/server/repository/AnswerRepository.java
+++ server/src/main/java/com/duokoala/server/repository/AnswerRepository.java
@@ -1,7 +1,6 @@
 package com.duokoala.server.repository;
 
 import com.duokoala.server.entity.Answer;
-import com.duokoala.server.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
