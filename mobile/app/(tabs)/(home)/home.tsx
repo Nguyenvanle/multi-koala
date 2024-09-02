f521b653e5ea9e8b8a0fdf8427c8959d35c5d587
Tú Lê <leduonganhtu0102@gmail.com>
2024-09-02 18:06:50 +0700

    add/mobile: home page (10%)

--- mobile/app/(tabs)/(home)/home.tsx
+++ mobile/app/(tabs)/(home)/home.tsx
@@ -1,16 +1,109 @@
 import { View, Text, SafeAreaView, StatusBar } from "react-native";
 import React from "react";
 import CircleStyle from "@/components/common/CircleStyle";
-import { Styles } from "@/constants/Styles";
+import { button, Styles, text } from "@/constants/Styles";
 import { Colors } from "@/constants/Colors";
+import Button from "@/components/common/Button";
+import { router } from "expo-router";
 
 const Home = () => {
   return (
-    <SafeAreaView style={Styles.container}>
+    <SafeAreaView
+      style={{
+        ...Styles.container,
+        flex: 1,
+        justifyContent: "space-evenly",
+      }}
+    >
       <StatusBar barStyle={"dark-content"} />
-
       <CircleStyle />
-      <View style={{ height: 250, backgroundColor: Colors.black,  }}></View>
+      <View
+        style={{
+          backgroundColor: Colors.teal_dark,
+          borderRadius: 20,
+          width: 420,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            alignSelf: "center",
+            marginVertical: 20,
+          }}
+        >
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginHorizontal: 20,
+            }}
+          >
+            <Text style={text.h4}>Welcome</Text>
+            <Text style={{ ...text.h3, color: Colors.teal_light }}>User</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignSelf: "center",
+              paddingHorizontal: 10,
+            }}
+          >
+            <Button
+              title="Sign In"
+              onPress={() => router.replace("/(auth)/sign-in")}
+              buttonStyle={{
+                ...button.Authen,
+                backgroundColor: Colors.dark,
+                width: 100,
+                borderRadius: 15,
+                marginHorizontal: 5,
+                borderColor: Colors.white,
+                borderWidth: 1,
+                marginTop: 0,
+              }}
+              textStyle={{ ...text.p, color: Colors.white }}
+            />
+            <Button
+              title="Sign Up"
+              onPress={() => router.replace("/(auth)/sign-up")}
+              buttonStyle={{
+                ...button.Authen,
+                backgroundColor: Colors.white,
+                width: 100,
+                borderRadius: 15,
+                marginHorizontal: 5,
+                borderColor: Colors.dark,
+                borderWidth: 1,
+                marginTop: 0,
+              }}
+              textStyle={{ ...text.p, color: Colors.black }}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            borderRadius: 10,
+            backgroundColor: Colors.teal_light,
+            justifyContent: "center",
+            alignItems: "center",
+            margin: 15,
+          }}
+        >
+          <View
+            style={{
+              alignSelf: "baseline",
+              margin: 20,
+              borderColor: Colors.black,
+              height: 30,
+            }}
+          >
+            <Text style={{ ...text.p, color: Colors.white }}>
+              Continue Learning
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View style={{ height: 250, backgroundColor: Colors.black }}></View>
     </SafeAreaView>
   );
 };
