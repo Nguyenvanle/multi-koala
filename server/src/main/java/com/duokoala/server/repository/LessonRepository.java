94aafaa9cb0b6a84267034b3782e1e6044ddf309
khachhoangthienn <tiendat29082003@gmail.com>
2024-09-07 22:05:32 +0700

    refactor/server: add sql count lesson by courseId

--- server/src/main/java/com/duokoala/server/repository/LessonRepository.java
+++ server/src/main/java/com/duokoala/server/repository/LessonRepository.java
@@ -2,8 +2,15 @@ package com.duokoala.server.repository;
 
 import com.duokoala.server.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, String> {
+    @Query(nativeQuery = true,
+            value = "SELECT COUNT(*) " +
+                    "FROM lesson " +
+                    "WHERE course_course_id = :courseId")
+    int countLessonsByCourseId(@Param("courseId") String courseId);
 }
