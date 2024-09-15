47180a42a7ec1ea8bf8e287c72fad477e9d7d022
khachhoangthienn <tiendat29082003@gmail.com>
2024-09-15 12:59:45 +0700

    refactor/server: refactor service content in sent gmail

--- server/src/main/java/com/duokoala/server/service/EmailService.java
+++ server/src/main/java/com/duokoala/server/service/EmailService.java
@@ -7,7 +7,6 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
