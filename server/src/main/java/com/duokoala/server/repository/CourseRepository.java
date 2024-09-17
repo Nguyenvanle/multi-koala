abc6bea49e498c58f09a490295cd1389b52a4c50
khachhoangthienn <tiendat29082003@gmail.com>
2024-09-17 13:33:09 +0700

    refactor/server: refactor service get my enroll Course

--- server/src/main/java/com/duokoala/server/repository/CourseRepository.java
+++ server/src/main/java/com/duokoala/server/repository/CourseRepository.java
@@ -23,7 +23,7 @@ public interface CourseRepository extends JpaRepository<Course, String> {
                     "FROM course c JOIN enroll_course ec " +
                     "ON c.course_id = ec.course_course_id " +
                     "WHERE ec.student_user_id = :studentId")
-    List<Course> findAllByUploadedByStudentId(@Param("studentId") String studentId);
+    List<Course> findAllEnrollCourseByStudentId(@Param("studentId") String studentId);
     List<Course> findAllByUploadedByTeacher(Teacher teacher);
     List<Course> findAllByStatus(Status status);
 }
