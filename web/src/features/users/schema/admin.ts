021156136db257baeecdeebddbced5577ed4fe2f
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-09-18 15:35:51 +0700

    add/web: certificate res

--- web/src/features/users/schema/admin.ts
+++ web/src/features/users/schema/admin.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { UserBodyType } from "@/features/users/schema/user";
 
 // Định nghĩa schema cho Admin
-export const AdminBodyType = UserBodyType.extend({
+export const AdminBodySchema = UserBodyType.extend({
   createByAdmin: z.string().nullable(),
   firstLogin: z.boolean(),
   deleted: z.boolean(),
 });
 
-export type AdminBodyType = z.infer<typeof UserBodyType>;
+export type AdminBodyType = z.infer<typeof AdminBodySchema>;
--- web/src/features/users/schema/teacher.ts
+++ web/src/features/users/schema/teacher.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { UserBodyType } from "@/features/users/schema/user";
 
 // Định nghĩa schema cho Admin
-export const TeacherBodyType = UserBodyType.extend({
+export const TeacherBodySchema = UserBodyType.extend({
   teacherRating: z.number(),
   firstLogin: z.boolean(),
   deleted: z.boolean(),
 });
 
-export type TeacherBodyType = z.infer<typeof TeacherBodyType>;
+export type TeacherBodyType = z.infer<typeof TeacherBodySchema>;
