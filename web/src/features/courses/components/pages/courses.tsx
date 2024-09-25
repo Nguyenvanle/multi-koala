4ca32fad9927970646d70be19f0cc0bb8f139841
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-09-25 10:35:30 +0700

    refactor/web: sv-props to swr course

--- web/src/features/courses/components/pages/courses.tsx
+++ web/src/features/courses/components/pages/courses.tsx
@@ -1,63 +1,18 @@
-// src/features/courses/pages/CoursesPage.tsx
 "use client";
 
-import { useState } from "react";
 import { CoursesTemplate } from "@/features/courses/components/layout/courses";
-import { CoursesResultResType } from "@/features/courses/types/course";
-import { GetServerSideProps } from "next";
-import { CourseFacade } from "@/features/courses/services/course-facade";
-import { CourseRepository } from "@/features/courses/services/course-repository";
-import { SortOption } from "@/features/courses/components/molecules/select-sort";
-import { useFilter } from "@/features/filter/hooks/useFilter";
-import useSWR from "swr";
+import { useCourses } from "@/features/courses/hooks/useCourses";
 
-export interface CoursesProps {
-  initialCourses: CoursesResultResType;
-}
+export default function Courses() {
+  const { courses, isLoading, isError, setSortOrder } = useCourses();
 
-export const getServerSideProps: GetServerSideProps<
-  CoursesProps
-> = async () => {
-  const courseFacade = new CourseFacade(new CourseRepository());
-  const initialCourses = await courseFacade.getProcessedCourses(
-    "rating_desc",
-    {}
-  );
-
-  return {
-    props: {
-      initialCourses,
-    },
-  };
-};
-
-const fetcher = async (url: string, sortOrder: SortOption, filters: any) => {
-  const courseFacade = new CourseFacade(new CourseRepository());
-  return courseFacade.getProcessedCourses(sortOrder, filters);
-};
-
-export default function Courses({ initialCourses }: CoursesProps) {
-  const [sortOrder, setSortOrder] = useState<SortOption>("rating_desc");
-  const { filters } = useFilter();
-
-  const {
-    data: courses,
-    error,
-    isLoading: loading,
-  } = useSWR(
-    ["/api/courses", sortOrder, filters],
-    () => fetcher("/api/courses", sortOrder, filters),
-    { fallbackData: initialCourses }
-  );
-
-  if (error) return <div>ERROR: {error}</div>;
+  if (isError) return <div>Error loading courses</div>;
 
   return (
     <CoursesTemplate
-      courses={courses}
-      loading={loading}
+      courses={courses || []}
+      loading={isLoading}
       setSortOrder={setSortOrder}
     />
   );
-};
-
+}
