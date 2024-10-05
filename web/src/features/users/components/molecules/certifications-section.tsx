56630f0617f68d2e7187141558bbb921eb4585ec
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-10-05 13:27:28 +0700

    fix/web: teacher ui done

--- web/src/features/users/components/molecules/certifications-section.tsx
+++ web/src/features/users/components/molecules/certifications-section.tsx
@@ -9,32 +9,58 @@ import {
 import { CertificationsResult } from "@/features/certification/types/certification-res";
 import { Award } from "lucide-react";
 import { formatDate } from "@/lib/utils";
+import { CertificationListEmpty } from "@/features/courses/components/molecules/certification-list-empty";
 
 export const CertificationsSection: React.FC<{
-  certifications: CertificationsResult;
-}> = ({ certifications }) => (
-  <div className="p-4 xl:p-6 pt-0 pb-6">
-    <Card className="bg-background rounded-lg shadow-lg">
-      <CardHeader className="px-6 py-4 border-b">
-        <h3 className="text-2xl font-bold flex items-center">
-          <Award className="mr-2 text-primary" size={24} /> Certifications
-        </h3>
-      </CardHeader>
-      <CardFooter className="p-6 flex">
-        <div className="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
-          {certifications.map((cert, index) => (
-            <Card key={index} className="">
-              <CardHeader>
-                <CardTitle>{cert.certificateName}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="mt-2">Issued by: {cert.issuingOrganization}</p>
-                <p>Date: {formatDate(cert.issueDate.toString())}</p>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </CardFooter>
-    </Card>
-  </div>
-);
+  certifications: CertificationsResult | null; // Cho phép certifications là null
+}> = ({ certifications }) => {
+  // Kiểm tra nếu certifications không có hoặc là mảng rỗng
+  if (!certifications || certifications.length === 0) {
+    return (
+      <div className="p-4 xl:p-6 pt-0 pb-6">
+        <Card className="bg-background rounded-lg shadow-lg">
+          <CardHeader className="px-6 py-4 border-b">
+            <h3 className="text-2xl font-bold flex items-center">
+              <Award className="mr-2 text-primary" size={24} /> Certifications
+            </h3>
+          </CardHeader>
+          <CardFooter className="p-6 flex">
+            <CertificationListEmpty
+              title="No Certifications Found"
+              message="It seems like you haven't received any certifications yet. Start your journey now!"
+              buttonText="Browse Certification Options"
+              redirect="#"
+            />
+          </CardFooter>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4 xl:p-6 pt-0 pb-6">
+      <Card className="bg-background rounded-lg shadow-lg">
+        <CardHeader className="px-6 py-4 border-b">
+          <h3 className="text-2xl font-bold flex items-center">
+            <Award className="mr-2 text-primary" size={24} /> Certifications
+          </h3>
+        </CardHeader>
+        <CardFooter className="p-6 flex">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
+            {certifications.map((cert, index) => (
+              <Card key={index} className="">
+                <CardHeader>
+                  <CardTitle>{cert.certificateName}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="mt-2">Issued by: {cert.issuingOrganization}</p>
+                  <p>Date: {formatDate(cert.issueDate.toString())}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
--- web/src/features/users/components/molecules/teacher-detail-section.tsx
+++ web/src/features/users/components/molecules/teacher-detail-section.tsx
@@ -14,45 +14,53 @@ import { formatDate } from "@/lib/utils";
 
 export const TeacherDetailsSection: React.FC<{ teacher: TeacherBodyType }> = ({
   teacher,
-}) => (
-  <Card className="col-span-1 md:col-span-2 shadow-lg">
-    <CardHeader className="border-b pb-4 rounded-t">
-      <CardTitle className="text-2xl font-bold flex items-center">
-        <User className="mr-2 text-primary" size={24} /> Teacher Information
-      </CardTitle>
-    </CardHeader>
-    <CardContent className="pt-6 border-b">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <InfoItem icon={Mail} label="Email" value={teacher.email} />
-        <InfoItem
-          icon={Calendar}
-          label="Birth Date"
-          value={
-            teacher.userBirth
-              ? formatDate(teacher.userBirth?.toString())
-              : "Not specified"
-          }
-          className={!teacher.userBirth ? "text-muted-foreground" : ""}
-        />
-        <InfoItem
-          icon={MapPin}
-          label="Hometown"
-          value={teacher.userHometown || "Not specified"}
-          className={!teacher.userHometown ? "text-muted-foreground" : ""}
-        />
-        <TeacherRatingItem
-          initialData={{ avgteacherRating: 0 }}
-          teacherId={teacher.userId}
-        />
-      </div>
-    </CardContent>
-    <CardFooter className="pt-6">
-      <div className="col-span-1 lg:col-span-2">
-        <h3 className="text-lg font-semibold flex items-center mb-2">
-          <Book className="mr-2 text-primary" size={20} /> Bio
-        </h3>
-        <p className="text-muted-foreground">{teacher.userBio}</p>
-      </div>
-    </CardFooter>
-  </Card>
-);
+}) => {
+  const avgRating = teacher.teacherRating || 0; // Assuming avgteacherRating is in teacher object
+
+  return (
+    <Card className="col-span-1 md:col-span-2 shadow-lg">
+      <CardHeader className="border-b pb-4 rounded-t">
+        <CardTitle className="text-2xl font-bold flex items-center">
+          <User className="mr-2 text-primary" size={24} /> Teacher Information
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="pt-6 border-b">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <InfoItem icon={Mail} label="Email" value={teacher.email} />
+          <InfoItem
+            icon={Calendar}
+            label="Birth Date"
+            value={
+              teacher.userBirth
+                ? formatDate(teacher.userBirth?.toString())
+                : "Not specified"
+            }
+            className={!teacher.userBirth ? "text-muted-foreground" : ""}
+          />
+          <InfoItem
+            icon={MapPin}
+            label="Hometown"
+            value={teacher.userHometown || "Not specified"}
+            className={!teacher.userHometown ? "text-muted-foreground" : ""}
+          />
+          <div className="flex flex-col">
+            <TeacherRatingItem
+              initialData={{ avgteacherRating: avgRating }}
+              teacherId={teacher.userId}
+            />
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className="pt-6">
+        <div className="col-span-1 lg:col-span-2">
+          <h3 className="text-lg font-semibold flex items-center mb-2">
+            <Book className="mr-2 text-primary" size={20} /> Bio
+          </h3>
+          <p className="text-muted-foreground">
+            {teacher.userBio || "Not specified"}
+          </p>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
