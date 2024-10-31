4fa6c570e6d90e72e3a16f4794e27b595d68cea7
Nguyenvanle <116999429+Nguyenvanle@users.noreply.github.com>
2024-10-31 18:10:13 +0700

    add/web: row selection

--- web/src/features/table/components/atoms/columns.tsx
+++ web/src/features/table/components/atoms/columns.tsx
@@ -13,8 +13,31 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export const columns: ColumnDef<Payment>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     accessorKey: "status",
     header: "Status",
--- web/src/features/table/components/atoms/data-table.tsx
+++ web/src/features/table/components/atoms/data-table.tsx
@@ -47,147 +47,154 @@ export function DataTable<TData, TValue>({
   );
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({});
+    const [rowSelection, setRowSelection] = React.useState({});
 
-  // Tạo một table state bằng tanstack table
-  const table = useReactTable({
-    data, // table data
-    columns, // columns data
-    getCoreRowModel: getCoreRowModel(), // tính toán và tạo 1 table model state
-    getPaginationRowModel: getPaginationRowModel(), // tính toán và tạo 1 table pagination model state
-    // Sorting
-    onSortingChange: setSorting, // nhận hành dộng người dùng ấn sort
-    getSortedRowModel: getSortedRowModel(), // tính toán và tạo 1 table sorted model state
-    onColumnFiltersChange: setColumnFilters,
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    state: {
-      sorting, // đưa sorting state vào tanstack table state
-      columnFilters,
-      columnVisibility,
-    },
-  });
+    // Tạo một table state bằng tanstack table
+    const table = useReactTable({
+      data, // table data
+      columns, // columns data
+      getCoreRowModel: getCoreRowModel(), // tính toán và tạo 1 table model state
+      getPaginationRowModel: getPaginationRowModel(), // tính toán và tạo 1 table pagination model state
+      // Sorting
+      onSortingChange: setSorting, // nhận hành dộng người dùng ấn sort
+      getSortedRowModel: getSortedRowModel(), // tính toán và tạo 1 table sorted model state
+      onColumnFiltersChange: setColumnFilters,
+      getFilteredRowModel: getFilteredRowModel(),
+      onColumnVisibilityChange: setColumnVisibility,
+      onRowSelectionChange: setRowSelection,
+      state: {
+        sorting, // đưa sorting state vào tanstack table state
+        columnFilters,
+        columnVisibility,
+        rowSelection,
+      },
+    });
 
-  return (
-    <div>
-      <div className="flex items-center py-4">
-        <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                );
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map(
-              (
-                headerGroup // Lấy mảng header cần hiển thị
-              ) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    // Lấy từng header cần hiển thị
-                    return (
-                      <TableHead key={header.id}>
-                        {header.isPlaceholder // xác định xem có phải cột giữ chỗ hay ko
-                          ? null // Không hiển thị
-                          : flexRender(
-                              // Hiển thị với các thuộc tính:
-                              header.column.columnDef.header, // Lấy id header
-                              header.getContext() // Lấy state header
-                            )}
-                      </TableHead>
-                    );
-                  })}
-                </TableRow>
-              )
-            )}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? ( // lấy mảng rows cần hiển thị
-              table.getRowModel().rows.map(
+    return (
+      <div>
+        <div className="flex items-center py-4">
+          <Input
+            placeholder="Filter emails..."
+            value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("email")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                Columns
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map(
                 (
-                  row // lấy từng row cần hiển thị
+                  headerGroup // Lấy mảng header cần hiển thị
                 ) => (
-                  <TableRow
-                    key={row.id} // lấy row id
-                    data-state={row.getIsSelected() && "selected"} // kiểm tra xem row có được chọn hay ko
-                  >
-                    {row.getVisibleCells().map(
-                      // chỉ lấy những hàng cần hiển thị (bỏ những ô không chưa thông tin hoặc bị ẩn do bộ lọc)
-                      (cell) => (
-                        <TableCell key={cell.id}>
-                          {flexRender(
-                            // hiển thị tương tự với header
-                            cell.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </TableCell>
-                      )
-                    )}
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      // Lấy từng header cần hiển thị
+                      return (
+                        <TableHead key={header.id}>
+                          {header.isPlaceholder // xác định xem có phải cột giữ chỗ hay ko
+                            ? null // Không hiển thị
+                            : flexRender(
+                                // Hiển thị với các thuộc tính:
+                                header.column.columnDef.header, // Lấy id header
+                                header.getContext() // Lấy state header
+                              )}
+                        </TableHead>
+                      );
+                    })}
                   </TableRow>
                 )
-              )
-            ) : (
-              // Fallback data
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+              )}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? ( // lấy mảng rows cần hiển thị
+                table.getRowModel().rows.map(
+                  (
+                    row // lấy từng row cần hiển thị
+                  ) => (
+                    <TableRow
+                      key={row.id} // lấy row id
+                      data-state={row.getIsSelected() && "selected"} // kiểm tra xem row có được chọn hay ko
+                    >
+                      {row.getVisibleCells().map(
+                        // chỉ lấy những hàng cần hiển thị (bỏ những ô không chưa thông tin hoặc bị ẩn do bộ lọc)
+                        (cell) => (
+                          <TableCell key={cell.id}>
+                            {flexRender(
+                              // hiển thị tương tự với header
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </TableCell>
+                        )
+                      )}
+                    </TableRow>
+                  )
+                )
+              ) : (
+                // Fallback data
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <div className="flex-1 text-sm text-muted-foreground">
+            {table.getFilteredSelectedRowModel().rows.length} of{" "}
+            {table.getFilteredRowModel().rows.length} row(s) selected.
+          </div>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div>
-    </div>
-  );
+    );
 }
