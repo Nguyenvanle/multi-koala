417241d2c63fc1bb2fb436c90b09bbb103b7e0d8
khachhoangthienn <tiendat29082003@gmail.com>
2024-11-14 14:30:41 +0700

    add/server: add new attribute lastUpdate into EnrollCourse

--- server/src/main/java/com/duokoala/server/entity/EnrollCourse.java
+++ server/src/main/java/com/duokoala/server/entity/EnrollCourse.java
@@ -1,4 +1,5 @@
 package com.duokoala.server.entity;
+
 import com.duokoala.server.entity.user.Student;
 import jakarta.persistence.*;
 import lombok.*;
@@ -17,6 +18,7 @@ public class EnrollCourse {
     @GeneratedValue(strategy = GenerationType.UUID)
     String enrollCourseId;
     LocalDateTime enrollAt;
+    LocalDateTime lastUpdate;
     float process;
     @ManyToOne
     Student student;
